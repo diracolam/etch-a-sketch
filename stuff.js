0b144d29954f113621e3105aba41fa3b17a05538
diracolam <54879705+diracolam@users.noreply.github.com>
2021-06-30 13:23:04 -0700

    Create stuff.js

---
+++ stuff.js
@@ -0,0 +1,15 @@
+const container = document.getElementById("container");
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "grid-item";
+    cell.onmouseover = function() {
+      this.style.background = "black";
+    }
+  }
+}
+
+makeRows(16, 16);
